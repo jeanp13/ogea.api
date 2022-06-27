8b9312968a1aa8a091dfdf5865cd956735cd70b4
Jean Paulo <jeanpauloalves@gmail.com>
2022-06-27 13:16:06 -0300

    api

--- src/configuration/upload.ts
+++ src/configuration/upload.ts
@@ -1,9 +1,7 @@
 import * as path from 'path';
 import * as crypto from 'crypto';
-import { StorageEngine } from 'multer';
-// import * as multer from 'multer';
-
-import multer = require('multer');
+import multer, { StorageEngine } from 'multer';
+// import multer = require('multer');
 
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
