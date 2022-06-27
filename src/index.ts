9f95dddfbb3f427e94e8c1e64ad6314192ffb300
Jean Paulo <jeanpauloalves@gmail.com>
2022-06-27 12:06:11 -0300

    api

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,24 @@
-import * as express from "express";
-import { Request, Response } from "express";
-import "express-async-errors";
-import { AppDataSource } from "./data-source";
-import config from "./configuration/config";
-import uploadConfig from "./configuration/upload";
+import * as express from 'express';
+import { Request, Response } from 'express';
+import 'express-async-errors';
+import * as cors from 'cors';
+import { AppDataSource } from './data-source';
+import config from './configuration/config';
+import uploadConfig from './configuration/upload';
 // import { Routes } from "./routes";
-import routes from "./routes";
-import AppError from "./errors/AppError";
+import routes from './routes';
+import AppError from './errors/AppError';
 // import AppError from "./errors/AppError";
 // import auth from "./configuration/auth";
 // import auth from "./middleware/auth";
 
-import "./providers";
+import './providers';
 
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
-app.use("/files", express.static(uploadConfig.uploadsFolder));
+app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
 
 app.use(
@@ -25,29 +26,29 @@ app.use(
     err: Error,
     request: Request,
     response: Response,
-    _: express.NextFunction
+    _: express.NextFunction,
   ) => {
     if (err instanceof AppError) {
       return response.status(err.statusCode).json({
-        status: "error",
+        status: 'error',
         message: err.message,
       });
     }
     console.error(err);
 
     return response.status(500).json({
-      status: "error",
-      message: "Internal Server Error",
+      status: 'error',
+      message: 'Internal Server Error',
     });
-  }
+  },
 );
 
 app.listen(config.port, async () => {
   console.log(`API has started on port ${config.port}`);
   try {
     await AppDataSource.initialize();
-    console.log("Database connected");
+    console.log('Database connected');
   } catch (error) {
-    console.log("Database not connected");
+    console.log('Database not connected');
   }
 });
